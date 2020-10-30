9a67e64a31fd59759397a2d95498e4af321e0fd4
Daniel Koch <dkoch@royal-26.cs.wisc.edu>
2020-10-30 13:05:25 -0500

    Added subclasses to Sym
    Signed-off-by: Daniel Koch <dkoch@royal-26.cs.wisc.edu>

--- Sym.java
+++ Sym.java
@@ -1,8 +1,12 @@
+import java.util.*;
+
 public class Sym {
     private String type;
+    private String ID;
     
-    public Sym(String type) {
+    public Sym(String type, String ID) {
         this.type = type;
+	this.ID = ID;
     }
     
     public String getType() {
@@ -11,5 +15,56 @@ public class Sym {
     
     public String toString() {
         return type;
+    }
+}
+
+class FnSym extends Sym{
+    private List<String> params;
+
+    public FnSym(String type, List<String> params, String ID) {
+	super(type, ID);
+	this.params = params;
+    }
+
+    public List<String> getParams() {
+	return params;
+    }
+
+    public String toString() {
+	return "FIXME";
+    }
+}
+
+class StructDefSym extends Sym {
+    private List<String> fields;
+
+    public StructDefSym(String type, List<String> fields, String ID) {
+	super(type, ID);
+	this.fields = fields;
+    }
+
+    public List<String> getFields() {
+	return fields;
+    }
+
+    public String toString() {
+	return "FIXME";
+    }
+}
+
+class StructSym extends Sym {
+    private List<String> params;
+
+    public StructSym(String type, List<String> params, String ID) {
+	super(type, ID);
+	this.params = params;
+    }
+
+    public List<String> getParams() {
+	return params;
+    }
+
+    public String toString() {
+	return "FIXME";
     }
 }
